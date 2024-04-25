1f73052fe0f0ab613764db56544a272429faf905
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2024-04-25 02:32:47 -0400

    animations and transitions on the home page

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+function mostrarFondo() {
+    var fondo = document.getElementById("fondo");
+    fondo.classList.add("mostrar");
+
+    var menuUber = document.querySelectorAll(".uberMenu");
+
+    menuUber.forEach(function (uberMenu) {
+        uberMenu.classList.add("uberMenu-show");
+    });
+
+    var menu = document.querySelectorAll(".menu");
+
+    menu.forEach(function (menu) {
+        menu.classList.add("menu-hiden");
+    });
+}
+
+function ocultarFondo() {
+    var fondo = document.getElementById("fondo");
+    fondo.classList.remove("mostrar");
+
+    var menuUber = document.querySelectorAll(".uberMenu");
+
+    menuUber.forEach(function (uberMenu) {
+        uberMenu.classList.remove("uberMenu-show");
+    });
+
+    var menu = document.querySelectorAll(".menu");
+
+    menu.forEach(function (menu) {
+        menu.classList.remove("menu-hiden");
+    });
+}
